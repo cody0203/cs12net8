02b3a580d8355d9e49369b93ec6212555ab94b44
Dinh Pham Cong <dinhphamcong@192.168.1.130>
2024-07-19 21:19:28 +0700

    chapter2: Async Console

---
+++ chapter02/AsyncAwait/Program.cs
@@ -0,0 +1,5 @@
+﻿HttpClient client = new();
+
+HttpResponseMessage response = await client.GetAsync("https://apple.com");
+
+WriteLine($"Apple's home page has {response.Content.Headers.ContentLength:N0} bytes");
