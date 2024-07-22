334d583e62f028fdf22131a9ca509381358efc22
Dinh Pham <dinh.pham@optimizely.com>
2024-07-22 15:58:17 +0700

    chapter05: Storing data in fields

---
+++ chapter05/PacktLibraryModern/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Packt.Shared;
+
+public class Book
+{
+    // .Net 7 and later as well as C#11 and later
+    public required string? Isbn;
+    public required string? Title;
+
+    // any verion of .Net
+    public string? Author;
+    public int PageCount;
+
+    public Book() {}
+
+    [SetsRequiredMembers]
+    public Book(string? Isbn, string? Title)
+    {
+        this.Isbn = Isbn;
+        this.Title = Title;
+    }
+}
