b2e8b62c9fd23c985c2268595cfffaa4efd54a22
DinhPham <dinh.pham@optimizely.com>
2024-07-30 16:57:32 +0700

    chapter06: Null Handling

---
+++ chapter06/NullHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿int thisCannotBeNull = 4;
+// thisCannotBeNull = null; // Compiler error
+
+int? thisCouldBeNull = null;
+
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault()); // 0
+
+thisCouldBeNull = 7;
+
+WriteLine(thisCouldBeNull); // 7
+WriteLine(thisCouldBeNull.GetValueOrDefault()); // 7
+
+
+// The actual type of int? is Nullable<int>.
+Nullable<int> thisCouldAlsoBeNull = null;
+thisCouldAlsoBeNull = 9;
+WriteLine(thisCouldAlsoBeNull);
