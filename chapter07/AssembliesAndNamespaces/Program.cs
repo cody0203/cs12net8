4baeb58fd5de6e6d8f584a4bcfc26dd3a19aeb1f
DinhPham <dinh.pham@optimizely.com>
2024-08-08 14:33:38 +0700

    chapter07: Understanding .NET components

---
+++ chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // Import the System namespace with using a using statement that will fix the error below.
+
+#region Relating C# keywords to .NET types
+string s1 = "Hello";
+String s2 = "World"; // It will be error if we remove System from csproj by <Using Remove="System" />
+WriteLine($"{s1} {s2}");
+
+#endregion
+
+#region Understanding native-sized integers
+WriteLine($"Environment.Is64BitProcess = {Environment.Is64BitProcess}");
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
+#endregion
