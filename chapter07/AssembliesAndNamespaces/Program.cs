15da0092c849e6b06976a6580a036889cddcf6f5
DinhPham <dinh.pham@optimizely.com>
2024-08-08 15:28:54 +0700

    chapter07: Build and Publish a NuGet Package

--- chapter07/AssembliesAndNamespaces/Program.cs
+++ chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System; // Import the System namespace with using a using statement that will fix the error below.
+using Packt.Shared;
 
 #region Relating C# keywords to .NET types
 string s1 = "Hello";
@@ -11,4 +12,18 @@ WriteLine($"{s1} {s2}");
 WriteLine($"Environment.Is64BitProcess = {Environment.Is64BitProcess}");
 WriteLine($"int.MaxValue = {int.MaxValue:N0}");
 WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
+#endregion
+
+#region SharedLibrary as NuGet Package
+Write("Enter a color value in hex:");
+string? hex = ReadLine();
+WriteLine($"Is {hex} a valid color value? {hex.IsValidHex()}");
+
+Write("Enter a XML element:");
+string? xmlTag = ReadLine();
+WriteLine($"Is {xmlTag} a valid XML element? {xmlTag.IsValidXmlTag()}");
+
+Write("Enter a password:");
+string? password = ReadLine();
+WriteLine($"Is {password} a valid password? {password.IsValidPassword()}");
 #endregion
