05188d53fa0ffdd0df79134a39eefb308b0a9553
DinhPham <dinh.pham@optimizely.com>
2024-09-09 11:30:21 +0700

    practicalApps: Implementing a middleware

--- practicalApps/Northwind.Web/Program.cs
+++ practicalApps/Northwind.Web/Program.cs
@@ -19,6 +19,32 @@ if (!app.Environment.IsDevelopment())
 }
 else
 
+// Implementing an anonnymous inline delegaste as middleware
+// to intercept HTTP requests and responses.
+
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+    RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+
+    if (rep is not null)
+    {
+        WriteLine($"Endpoint name: {rep.DisplayName}");
+        WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+    }
+
+    if (context.Request.Path == "/bonjour")
+    {
+        // In The acse of a match on URL path, this becomes a terminating
+        // delegate that returns so does not call the next delegate.
+
+        await context.Response.WriteAsync("Bonjour Monde!");
+        return;
+    }
+
+    // We could modify the request before calling the next delegate.
+    await next();
+});
+
 app.UseHttpsRedirection();
 app.UseDefaultFiles(); // index.html, default.html, and so on.
 app.UseStaticFiles();
