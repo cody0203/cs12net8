73a3da1030b4f1e7c1f4e2df5acd0e4855048a4f
DinhPham <dinh.pham@optimizely.com>
2024-09-10 21:43:23 +0700

    practicalApps: razor library

---
+++ practicalApps/Northwind.Razor.Employees/Areas/PacktFeatures/Pages/EmployeeList.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Northwind.EntityModels;
+
+namespace PacktFeatures.Pages;
+
+public class EmployeeListPageModel : PageModel
+{
+    private NorthwindContext _db;
+    
+    public EmployeeListPageModel(NorthwindContext db)
+    {
+        _db = db;
+    }
+
+    public Employee[] Employees { get; set; } = null!;
+
+    public void OnGet()
+    {
+        ViewData["Title"] = "Northwind B2B - Employees";
+
+        Employees = _db.Employees
+        .OrderBy(e => e.LastName)
+        .ThenBy(e => e.FirstName)
+        .ToArray();
+    }
+}
