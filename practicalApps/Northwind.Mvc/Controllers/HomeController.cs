94b648026197d1888a789ebe2e2c9186c2445ae0
DinhPham <dinh.pham@optimizely.com>
2024-09-13 14:05:06 +0700

    practicalApps: implementing entity and view models

--- practicalApps/Northwind.Mvc/Controllers/HomeController.cs
+++ practicalApps/Northwind.Mvc/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 using System.Diagnostics; // To use Controller, IActionResult.
 using Microsoft.AspNetCore.Mvc; // To use ErrorViewModel.
 using Northwind.Mvc.Models; // To use Activity.
+using Microsoft.EntityFrameworkCore;
+using Northwind.EntityModels; // To use Include method.
 
 namespace Northwind.Mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly NorthwindContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        NorthwindContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
@@ -21,7 +27,14 @@ public class HomeController : Controller
         // _logger.LogWarning("Second warning!");
         // _logger.LogInformation("I am in the Index method of the HomeController");
 
-        return View();
+        HomeIndexViewModel model = new
+        (
+            VisitorCount: Random.Shared.Next(1, 1001),
+            Categories: _db.Categories.ToList(),
+            Products: _db.Products.ToList()
+        );
+
+        return View(model); // Pass the model to the view.
     }
 
     public IActionResult Privacy()
@@ -33,5 +46,24 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult ProductDetail(int? id)
+    {
+        if (!id.HasValue)
+        {
+            return BadRequest("You must pass a product ID in the route, for example, /Home/ProductDetail/21");
+        }
+
+        Product? model = _db.Products
+        .Include(p => p.Category)
+        .SingleOrDefault(p => p.ProductId == id);
+
+        if (model is null)
+        {
+            return NotFound($"ProductId {id} not found.");
+        }
+
+        return View(model);
     }
 }
