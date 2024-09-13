2c9d098348696b056b0cdfc22fdf8db6f67a55f1
DinhPham <dinh.pham@optimizely.com>
2024-09-13 11:32:52 +0700

    practicalApps: initializing mvc app

---
+++ practicalApps/Northwind.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics; // To use Controller, IActionResult.
+using Microsoft.AspNetCore.Mvc; // To use ErrorViewModel.
+using Northwind.Mvc.Models; // To use Activity.
+
+namespace Northwind.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        // Logger types
+        // _logger.LogError("This is a serious error (not really!)");
+        // _logger.LogWarning("This is your first warning!");
+        // _logger.LogWarning("Second warning!");
+        // _logger.LogInformation("I am in the Index method of the HomeController");
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
